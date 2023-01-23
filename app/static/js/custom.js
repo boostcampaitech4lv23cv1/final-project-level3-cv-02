102c0e07a106b4b56ade40c6f045133499d90484
yjlee <yjlee.k94@gmail.com>
2023-01-23 16:05:28 +0900

    feat: drag&drop 기능 삭제

--- app/static/js/custom.js
+++ app/static/js/custom.js
@@ -1,25 +1,4 @@
+//TODO: 테스트 코드. 추후 삭제
 function hello(){
     alert('dd')
 }
-
-function preventDefaults(e) { //브라우저 파일 드래그&드롭 기본 이벤트 막기
-    e.preventDefault();
-    e.stopPropagation();
-}
-
-/*드래그 인 시 하이라이팅 & 드래그 아웃 시 하이라이팅 제거*/
-const dropArea = document.getElementById("drop-file");
-
-function highlight(e) {
-  preventDefaults(e);
-  dropArea.classList.add("highlight");
-}
-
-function unhighlight(e) {
-  preventDefaults(e);
-  dropArea.classList.remove("highlight");
-}
-
-dropArea.addEventListener("dragenter", highlight, false);
-dropArea.addEventListener("dragover", highlight, false);
-dropArea.addEventListener("dragleave", unhighlight, false);
