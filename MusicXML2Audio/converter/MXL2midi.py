81024fb29d4ef4d71221ed7e89d11989765a4695
byj9402 <byj9402@naver.com>
2023-01-24 04:25:43 +0000

    feat: MusicXML2Audio
    #2

---
+++ MusicXML2Audio/converter/MXL2midi.py
@@ -0,0 +1,22 @@
+import music21
+import os
+import fnmatch
+
+def MXL2MIDI(data_path, output_path) :
+
+    print("MXL to MIDI")
+
+    # save data list
+    matches = []
+    for root, dirnames, filenames in os.walk(data_path):
+            for filename in fnmatch.filter(filenames, '*.xml'):
+                matches.append(os.path.join(root, filename))
+
+    # convert musicxml to midi
+    for xml in matches:
+        file_name = os.path.splitext(os.path.basename(xml))[0]
+
+        c = music21.converter.parse(data_path + f'/{file_name}.xml')
+        c.write('mid', output_path + f'/{file_name}.mid')
+    
+    return
---
+++ MusicXML2Audio/main.py
@@ -0,0 +1,25 @@
+from converter.MXL2midi import MXL2MIDI
+from converter.midi2wav import MIDI2WAV
+from converter.wav2sound import WAV2MP3, WAV2FLAC
+
+#handle
+NAME = 'chopin'
+XML_PATH = './data/.xml'
+OUTPUT_PATH = './data/output'
+
+MERGE = True
+MP3 = True
+FLAC = False
+
+def main(NAME, XML_PATH, OUTPUT_PATH, MERGE, MP3, FLAC) :
+    
+    MXL2MIDI(XML_PATH, OUTPUT_PATH)
+    MIDI2WAV(NAME, OUTPUT_PATH, merge=MERGE)
+    if MP3 :
+        WAV2MP3(OUTPUT_PATH)
+    if FLAC :
+        WAV2FLAC(OUTPUT_PATH)
+    return
+
+if __name__ == "__main__" :
+    main(NAME, XML_PATH, OUTPUT_PATH, MERGE, MP3, FLAC)
