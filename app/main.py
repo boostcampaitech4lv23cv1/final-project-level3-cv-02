a766cd61cf4c132ba64f3af2dc41718ed7534968
byj9402 <byj9402@naver.com>
2023-01-27 04:47:08 +0000

    feat: add sign in/up page
    #11

--- app/main.py
+++ app/main.py
@@ -52,6 +52,18 @@ def error_form(request: Request) :
 @app.get("/error")
 def error_form(request: Request) :
     return templates.TemplateResponse('error.html', context={'request': request})
+@app.get("/sign-in")
+def file_form(request: Request): 
+    return templates.TemplateResponse('sign-in.html', context={'request': request})
+
+@app.get("/sign-up")
+def file_form(request: Request): 
+    return templates.TemplateResponse('sign-up.html', context={'request': request})
+
+@app.post('/play')
+def play_form(request:Request, images: List[bytes] = File(...)):
+    print({"file_sizes": [len(image) for image in images]})
+    return templates.TemplateResponse('play.html', context = {'request': request})
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
