5f27ff6411a3caa25f7ed9810a1ea92cb714a925
gusangmo <gusangmo0605@gmail.com>
2023-01-27 16:49:16 +0000

    feat: change main.py for model inference logic
     #13

--- app/main.py
+++ app/main.py
@@ -2,7 +2,33 @@ from fastapi import FastAPI, Request, File
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from typing import List
-import uvicorn
+import uvicorn 
+import sys 
+sys.path.append("..")
+from oemer.ete import main as predict 
+from oemer.ete import extract
+print("predict:", predict, "extract:", extract)
+import subprocess
+from PIL import Image 
+import io
+from argparse import Namespace
+
+### 이거 분리가 필요하면 constant.py 같은 곳에 별도로. oemer 옵션 지정하는 곳
+
+
+default_dict = dict(
+    img_path = "/opt/ml/tmp.png",
+    output_path = "./",
+    use_tf = False,
+    without_deskew = True,
+    save_cache = False
+)
+
+fast_only_dict = dict(thresh = 512, step_size = 256)
+slow_only_dict = dict(thresh = 1024,step_size = 128)
+
+fast_dict = {**default_dict, **fast_only_dict}
+slow_dict = {**default_dict, **slow_only_dict}
 
 app = FastAPI()
 templates = Jinja2Templates(directory='templates')
@@ -15,6 +41,17 @@ def file_form(request: Request):
 @app.post('/play')
 def play_form(request:Request, images: List[bytes] = File(...)):
     print({"file_sizes": [len(image) for image in images]})
+    
+    dict_args = Namespace(**fast_dict)
+
+    for image_byte in images: 
+        print("type of image;", type(image_byte))
+        image = Image.open(io.BytesIO(image_byte))
+        image.save("/opt/ml/tmp.png")
+        print("Successfully stored") 
+        result_xml = extract(dict_args)  
+        print("result_xml:", result_xml)
+        print("type of result_xml:", type(result_xml))
     return templates.TemplateResponse('play.html', context = {'request': request})
 
 if __name__ == '__main__':
