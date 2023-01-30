b42ba98cb9b96061c22c054d24bd78ed1e569a5b
byj9402 <byj9402@naver.com>
2023-01-30 12:40:07 +0000

    design: play page
    #8

--- app/main.py
+++ app/main.py
@@ -19,9 +19,10 @@ def file_form(request: Request):
 
 #(TODO) 인증 기능 구현 후 play에 query_parameter, path_parameter해서 유저별 페이지 만들기 
 # ex. /play?user_id=sangmo
-@app.post('/play')  #필요 시 JinjaTemplate으로 path paramter로 받아서 
-def play_form(request:Request):
-    return templates.TemplateResponse('play.html', context={'request': request})
+@app.post('/play')
+def play_form(request:Request, images: List[bytes] = File(...)):
+    print({"file_sizes": [len(image) for image in images]})
+    return templates.TemplateResponse('play.html', context = {'request': request})
 
 #(TODO) 지금은 img_path를 함수 인자로 안 받고 있지만, REST API에서 img_path를 받을 수 있다면, 거기에 접근해서 img_path를 가져올 수 있게끔 하기]
 #(TODO 2) 에러 페이지 별도로 만들어서 띄우기... 근데 이거 나중에 해라
@@ -37,7 +38,6 @@ def predict_model(request: Request):
     print(results)
     return templates.TemplateResponse('play.html', context={'request': request, "results:" : results})
 
-
 #(TODO 1) /opt/ml/tmp/file(로컬 저장)을 전제로 하고 있는데, DB 저장 혹은 버킷 저장 시 경로를 인자로 받기
 #(TODO 2) print문 등을 logging으로 대체하기
 @app.post("/loading")
@@ -49,10 +49,6 @@ def loading_form(request: Request, images: List[bytes] = File(...)) :
 def error_form(request: Request) :
     return templates.TemplateResponse('error.html', context={'request': request})
 
-@app.get("/error")
-def error_form(request: Request) :
-    return templates.TemplateResponse('error.html', context={'request': request})
-    
 @app.get("/sign-in")
 def file_form(request: Request): 
     return templates.TemplateResponse('sign-in.html', context={'request': request})
@@ -65,10 +61,13 @@ def file_form(request: Request):
 def check_form(request: Request): 
     return templates.TemplateResponse('sign-check.html', context={'request': request})
 
-@app.post('/play')
-def play_form(request:Request, images: List[bytes] = File(...)):
-    print({"file_sizes": [len(image) for image in images]})
+@app.get('/play')
+def play_form(request:Request):
     return templates.TemplateResponse('play.html', context = {'request': request})
 
+@app.get('/error')
+def error_form(request: Request) :
+    return templates.TemplateResponse('error.html', context={'request': request})
+
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=80, reload=True)
